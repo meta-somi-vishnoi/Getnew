aa1b537cf5900cdaebc2a1d5a314bf2eae212f04
meta-somi-vishnoi <41181551+meta-somi-vishnoi@users.noreply.github.com>
2018-07-19 23:21:02 -0700

    Add files via upload

---
+++ Discount/Products.java
@@ -0,0 +1,135 @@
+package Discount;
+
+import java.util.ArrayList;
+
+class Products{
+	private ArrayList<Items> listOfCustomer = new ArrayList<Items>();
+	private ArrayList<Items> existingProducts = new ArrayList<Items>();
+	
+	public Products()
+	{
+		Items product1 = new Items(1,"Mobiles",10000,50);
+		Items product2 = new Items(2,"Cameras",5000,100);
+		Items product3 = new Items(3,"laptops",30000,20);
+		Items product4 = new Items(4,"watch",2000,10);
+		Items product5 = new Items(5,"Ipads",7000,20);
+		Items product6 = new Items(6,"Tablets",25000,70);
+		existingProducts.add(product1);
+		existingProducts.add(product2);
+		existingProducts.add(product3);
+		existingProducts.add(product4);
+		existingProducts.add(product5);
+	}
+	
+	public void addProduct(int itemId, int quantity)
+	{
+		int flag = 0;
+		int price;
+		String name; 
+		for(int j = 0; j < existingProducts.size(); j++) {
+			if(itemId == existingProducts.get(j).getId() && quantity <= existingProducts.get(j).getQuantity()) {
+				price = existingProducts.get(j).getPrice();
+				name = existingProducts.get(j).getName();
+				Items item = new Items(itemId, name, price, quantity);
+				listOfCustomer.add(item);
+				flag = 1;
+				existingProducts.get(j).updateQuantity(existingProducts.get(j).getQuantity() - quantity);
+			}
+		}
+		if(flag == 0) {
+                    System.out.println("Item cannot be added");
+		}
+	}
+	
+	public void removeProduct(int itemId)
+	{
+		int flag = 0;
+		for(int i = 0; i < listOfCustomer.size(); i++)	{
+			if(listOfCustomer.get(i).getId() == itemId) {
+				for(int j = 0; j < existingProducts.size(); j++) {
+					if(existingProducts.get(j).getId() == itemId) {
+							existingProducts.get(j).updateQuantity(existingProducts.get(j).getQuantity() + listOfCustomer.get(i).getQuantity());
+					}
+				}
+				listOfCustomer.remove(i);
+				flag = 1;
+			}
+		}
+		if(flag == 1) {
+			System.out.println("Item removed successfully"); 
+		}
+		else {
+			System.out.println("Item cannot be removed");
+		}
+	}
+	
+	public void updateQuantity(int itemId, int newQuantity)
+	{
+        int flag = 0;
+		for(int j = 0; j < existingProducts.size(); j++) {
+			if(existingProducts.get(j).getId() == itemId && existingProducts.get(j).getQuantity() >= newQuantity) {
+				for(int i = 0;i < listOfCustomer.size(); i++) {
+					if(listOfCustomer.get(i).getId() == itemId) {
+                        flag = 1;
+                        listOfCustomer.get(i).updateQuantity(newQuantity);
+						existingProducts.get(j).updateQuantity(existingProducts.get(j).getQuantity() - newQuantity);
+					}
+				}
+			}	
+		}
+        if(flag==0) {
+			System.out.println("Item cannot be updated.");
+        }
+	}
+	
+	public int piceOfProduct(int itemId)
+	{
+		for(int i = 0; i < listOfCustomer.size(); i++) {
+			if(listOfCustomer.get(i).getId() == itemId) {
+				return ((listOfCustomer.get(i).getQuantity()) * (listOfCustomer.get(i).getPrice()));
+			}
+			else {
+				System.out.println("Enter valid product Id");
+				return 0;
+			}
+	
+		}
+		return 0;
+	}
+	
+	public int totalPrice()
+	{
+		int sum = 0;
+		for(int i = 0; i < listOfCustomer.size(); i++)       
+		{
+			sum = sum + (listOfCustomer.get(i).getQuantity()) * (listOfCustomer.get(i).getPrice());
+		}
+		return sum;
+	}
+	
+	public void displayItems()
+	{
+		for(int i = 0; i < listOfCustomer.size(); i++) {
+			System.out.print("\n The " + ( i + 1 ) + " item is: \n" + listOfCustomer.get(i).getId() + "\t" + listOfCustomer.get(i).getName() + "\t" + listOfCustomer.get(i).getPrice() + "\t" + listOfCustomer.get(i).getQuantity() + "\n");	
+		}
+	}
+	
+	public void displayExistingItems()
+	{
+		for(int i = 0; i < existingProducts.size(); i++) {
+			System.out.print(" The " + ( i + 1 ) + " item is: \n" + existingProducts.get(i).getId() + "\t" + existingProducts.get(i).getName() + "\t" + existingProducts.get(i).getPrice() + "\t" + existingProducts.get(i).getQuantity() + "\n");	
+		}
+	}
+	
+	public int dicountOnProduct(int minimumPrice)
+	{
+		int sumOfProducts = 0;
+		for(int i = 0; i < listOfCustomer.size(); i++) {
+			if(listOfCustomer.get(i).getPrice() >= minimumPrice) {
+				sumOfProducts = sumOfProducts + ((listOfCustomer.get(i).getQuantity()) * (listOfCustomer.get(i).getPrice()));
+			}
+		}
+        return sumOfProducts;
+	}
+}
+
---
+++ Discount/PromotionOrder.java
@@ -0,0 +1,98 @@
+package Discount;
+
+import java.util.Scanner;
+
+public class PromotionOrder {
+	public static void main(String args[])
+	{
+		Products customerProducts = new Products();
+		fixedOrderPromotion order = new fixedOrderPromotion();
+		fixedProductPromotion product = new fixedProductPromotion();
+		int itemId,price,itemQuantity;
+		float totalDiscount;
+		String name,code;
+		System.out.println("Select options: \n press 1 to add product to cart \n press 2 to remove product from cart \n "
+				+ "press 3 to update quantity of product \n press 4 to get price of particular product"
+				+ "\n press 5 to get billing price \n press 6 to display list of products \n"
+                + "press 7 to enter promocode \n press 8 to exit \n");
+		do
+		{
+			System.out.println("The existing products are:");
+			customerProducts.displayExistingItems();
+			System.out.println("\n");
+			Scanner sc = new Scanner(System.in);
+			int option = sc.nextInt();
+			switch(option)
+			{
+			case 1: System.out.println("Enter Id of product");
+					itemId = sc.nextInt();
+					System.out.println("Enter quantity of product");
+					itemQuantity = sc.nextInt();
+					customerProducts.addProduct(itemId, itemQuantity);
+					break;
+			case 2: System.out.println("Enter Id of product");
+					itemId = sc.nextInt();
+					customerProducts.removeProduct(itemId);
+					break;
+			case 3: System.out.println("Enter Id of product");
+					itemId = sc.nextInt();
+					System.out.println("Enter new quantity of product");
+					itemQuantity = sc.nextInt();
+					customerProducts.updateQuantity(itemId,itemQuantity);
+					break;
+			case 4: System.out.println("Enter Id of product");
+					itemId = sc.nextInt();
+					System.out.println(" Total price of product is ");
+					int priceOfProduct = customerProducts.piceOfProduct(itemId);
+					System.out.println(priceOfProduct);
+					break;
+			case 5: System.out.print("Total billing price of product is ");
+                    int sumOfProducts = customerProducts.totalPrice() + product.getFixedDiscount(customerProducts) + order.getFixedDiscount(customerProducts);
+					System.out.println(sumOfProducts);
+					break;
+			case 6: System.out.print("list of products is : \n");
+					customerProducts.displayItems();
+					break;	
+			case 8: System.exit(0);
+			case 7: System.out.println("Enter Promocode");
+					code = sc.next();
+					if(order.isPromotionApplicable(code)) {
+						System.out.println("Discount available on fixed order is:");
+						int discount, minimumAmount;
+						minimumAmount = 20000;
+                        discount = 25;
+						order.setMinimumPrice(minimumAmount);
+						if(order.getMinimumPrice(customerProducts) >= minimumAmount) {
+							order.setFixedDiscount(discount);
+						}
+						totalDiscount = discount * order.getMinimumPrice(customerProducts) / 100;
+                                                System.out.println(discount + "%");
+                                                System.out.println("Discount availed:" + totalDiscount);
+					}
+					else  {
+						System.out.println("No discount available on fixed order");
+                        if(product.isPromotionApplicable(code)) {
+                            System.out.println("Discount available on fixed product is:");
+                            int discount, minimumAmount;
+                            minimumAmount = 2000;
+                            discount = 20;
+                            product.setMinimumPrice(minimumAmount);
+                            int sumPrice = product.getMinimumPrice(customerProducts);
+                            product.setFixedDiscount(discount);
+                            totalDiscount = discount * sumPrice / 100;
+                            System.out.println(discount + "%");
+                            System.out.println("Discount availed:" + totalDiscount);
+                        }
+                        else {
+                            System.out.println("No discount available on fixed product");
+                        }
+                    }
+                    break;
+		    }
+			System.out.println("\n Choose option again: \n");
+		}while(true);
+		
+	}
+	
+}
+
