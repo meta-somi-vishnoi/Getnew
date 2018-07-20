6bced2e8e5ba5b9a89626d4f383c4be80706afb4
meta-somi-vishnoi <41181551+meta-somi-vishnoi@users.noreply.github.com>
2018-07-20 03:25:07 -0700

    Add files via upload

---
+++ RS-Project2/ShoppingCart/Items.java
@@ -0,0 +1,35 @@
+package ShoppingCart;
+
+class Items {
+	private int itemId;
+	private String name;
+	private int price;
+	private int quantity;
+
+	public Items(int itemId, String name, int price, int quantity) {
+		this.itemId = itemId;
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity;
+	}
+
+	public void updateQuantity(int itemQuantity) {
+		this.quantity = itemQuantity;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public int getId() {
+		return itemId;
+	}
+
+	public String getName() {
+		return name;
+	}
+}
---
+++ RS-Project2/ShoppingCart/PromotionOrder.java
@@ -0,0 +1,147 @@
+package ShoppingCart;
+
+import java.util.*;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+enum PromotionEnum {
+	PromoCode1("FAAVC", "22/05/2017", "22/12/2018"), PromoCode2("FAAVD",
+			"22/06/2018", "22/12/2018"), PromoCode3("FAAVE", "01/06/2018",
+			"01/12/2018");
+	String code;
+	String startDate;
+	String endDate;
+
+	PromotionEnum(String code, String startDate, String endDate) {
+		this.code = code;
+		this.startDate = startDate;
+		this.endDate = endDate;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public String getStartDate() {
+		return startDate;
+	}
+
+	public String getEndDate() {
+		return endDate;
+	}
+}
+
+interface PromotionOrder {
+	public int getMinimumPrice(Products product);
+
+	public void setMinimumPrice(int price);
+
+	public int getFixedDiscount(Products product);
+
+	public void setFixedDiscount(int discount);
+
+	public boolean isPromotionApplicable(String code);
+}
+
+class fixedProductPromotion implements PromotionOrder {
+	private int discount;
+	private int minimumPrice;
+
+	fixedProductPromotion() {
+		discount = 0;
+	}
+
+	public void setMinimumPrice(int minimumPrice) {
+		this.minimumPrice = minimumPrice;
+	}
+
+	public int getMinimumPrice(Products product) {
+		return product.discountOnProduct(minimumPrice);
+	}
+
+	public int getFixedDiscount(Products product) {
+		return (discount * (product.discountOnProduct(minimumPrice)) / 100);
+	}
+
+	public void setFixedDiscount(int discount) {
+		this.discount = discount;
+	}
+
+	public boolean isPromotionApplicable(String code) {
+		Date date = new Date();
+		for (PromotionEnum enumValue : PromotionEnum.values()) {
+			SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+			long start = 0;
+			long end = 0;
+			long current = 0;
+			try {
+				Date startDate = (Date) formatter.parse(enumValue
+						.getStartDate());
+				Date endDate = (Date) formatter.parse(enumValue.getEndDate());
+				start = startDate.getTime();
+				end = endDate.getTime();
+				current = date.getTime();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			if (enumValue.getCode().equals(code) && current >= start
+					&& current <= end) {
+				return true;
+			}
+		}
+		return false;
+	}
+}
+
+class fixedOrderPromotion implements PromotionOrder {
+	private int discount;
+	private int minimumPrice;
+
+	fixedOrderPromotion() {
+		discount = 0;
+	}
+
+	public int getMinimumPrice(Products product) {
+		return product.totalPrice();
+	}
+
+	public void setMinimumPrice(int minimumPrice) {
+		this.minimumPrice = minimumPrice;
+	}
+
+	public int getFixedDiscount(Products product) {
+		return discount * product.totalPrice() / 100;
+	}
+
+	public void setFixedDiscount(int discount) {
+		this.discount = discount;
+	}
+
+	public boolean isPromotionApplicable(String code) {
+
+		Date date = new Date();
+		for (PromotionEnum enumValue : PromotionEnum.values()) {
+			SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+			long start = 0;
+			long end = 0;
+			long current = 0;
+			try {
+				Date startDate = (Date) formatter.parse(enumValue
+						.getStartDate());
+				Date endDate = (Date) formatter.parse(enumValue.getEndDate());
+				start = startDate.getTime();
+				end = endDate.getTime();
+				current = date.getTime();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			if (enumValue.getCode().equals(code) && current >= start
+					&& current <= end) {
+				return true;
+			}
+		}
+		return false;
+	}
+}
