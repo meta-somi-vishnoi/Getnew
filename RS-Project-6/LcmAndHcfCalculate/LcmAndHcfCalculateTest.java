37c842b1dce90ddc610e09e07e063c2353558cbb
meta-somi-vishnoi <41181551+meta-somi-vishnoi@users.noreply.github.com>
2018-07-24 03:03:24 -0700

    Update LcmAndHcfCalculateTest.java

--- RS-Project-6/LcmAndHcfCalculate/LcmAndHcfCalculateTest.java
+++ RS-Project-6/LcmAndHcfCalculate/LcmAndHcfCalculateTest.java
@@ -2,65 +2,68 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class LcmAndHcfCalculatorTest {
-    LcmAndHcfCalculator calculator = new LcmAndHcfCalculator();
-    LcmAndHcfCalculator calculator1 = new LcmAndHcfCalculator();
-    LcmAndHcfCalculator calculator2 = new LcmAndHcfCalculator();
-    LcmAndHcfCalculator calculator3 = new LcmAndHcfCalculator();
-    LcmAndHcfCalculator calculator4 = new LcmAndHcfCalculator();
-
+public class LcmAndHcfCalculateTest {
+  
     @Test
-    public void testLcm1() {
+    public void LcmCalculateTesting1() {
         int result;
+        LcmAndHcfCalculator calculator1 = new LcmAndHcfCalculator();
         result = calculator1.lcmCalculator(20, 40);
         assertEquals(result, 40);
     }
 
     @Test
-    public void testLcm2() {
+    public void LcmCalculateTesting2() {
         int result;
+        LcmAndHcfCalculator calculator2 = new LcmAndHcfCalculator();
         result = calculator2.lcmCalculator(120, 90);
         assertEquals(result, 360);
     }
 
     @Test
-    public void testLcm3() {
+    public void LcmCalculateTesting3() {
         int result;
+        LcmAndHcfCalculator calculator3 = new LcmAndHcfCalculator();
         result = calculator3.lcmCalculator(25, 30);
         assertEquals(result, 150);
     }
 
     @Test
-    public void testLcm4() {
+    public void LcmCalculateTesting4() {
         int result;
+        LcmAndHcfCalculator calculator4 = new LcmAndHcfCalculator();
         result = calculator4.lcmCalculator(125, 30);
         assertNotEquals(result, 150);
     }
 
     @Test
-    public void testHcf1() {
+    public void HcfCalculateTesting1() {
         int result;
+        LcmAndHcfCalculator calculator = new LcmAndHcfCalculator();
         result = calculator.hcfCalculator(20, 40);
         assertEquals(result, 20);
     }
 
     @Test
-    public void testHcf2() {
+    public void HcfCalculateTesting2() {
         int result;
+        LcmAndHcfCalculator calculator = new LcmAndHcfCalculator();
         result = calculator.hcfCalculator(5, 2);
         assertEquals(result, 1);
     }
 
     @Test
-    public void testHcf3() {
+    public void HcfCalculateTesting3() {
         int result;
+        LcmAndHcfCalculator calculator = new LcmAndHcfCalculator();
         result = calculator.hcfCalculator(120, 90);
         assertEquals(result, 30);
     }
 
     @Test
-    public void testHcf4() {
+    public void HcfCalculateTesting4() {
         int result;
+        LcmAndHcfCalculator calculator = new LcmAndHcfCalculator();
         result = calculator.hcfCalculator(120, 45);
         assertNotEquals(result, 20);
     }
