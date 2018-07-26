11be4ba6b3bfbf6cb7c57332952313bd859e7cde
meta-somi-vishnoi <41181551+meta-somi-vishnoi@users.noreply.github.com>
2018-07-26 01:13:33 -0700

    Update ArrayFunctionsTest.java

--- RS/RS-Project-5/ArrayFunctions/ArrayFunctionsTest.java
+++ RS/RS-Project-5/ArrayFunctions/ArrayFunctionsTest.java
@@ -4,112 +4,117 @@ import org.junit.Test;
 
 public class ArrayFunctionsTest {
 
-    ArrayFunctions arrayTests = new ArrayFunctions();
-
     @Test
-    public void testMirrorInArray1() {
+    public void testMirrorInArrayWithReturnValue7() {
         int maximumMirrorValue;
-        maximumMirrorValue = arrayTests.maximumMirror(new int[] { 1, 2, 3, 4,
-                3, 2, 1 }, 7);
+        ArrayFunctions arrayTests = new ArrayFunctions();
+        maximumMirrorValue = arrayTests.maximumMirror(new int[] { 1, 2, 3, 4, 3, 2, 1 }, 7);
         assertEquals(maximumMirrorValue, 7);
     }
 
     @Test
-    public void testMirrorInArray2() {
+    public void testMirrorInArrayWithReturnValue3() {
         int maximumMirrorValue;
-        maximumMirrorValue = arrayTests.maximumMirror(new int[] { 1, 2, 1, 4 },
-                4);
+        ArrayFunctions arrayTests = new ArrayFunctions();
+        maximumMirrorValue = arrayTests.maximumMirror(new int[] { 1, 2, 1, 4 }, 4);
         assertEquals(maximumMirrorValue, 3);
     }
 
     @Test
-    public void testMirrorInArray3() {
+    public void testMirrorInArrayWithReturnValue() {
         int maximumMirrorValue;
-        maximumMirrorValue = arrayTests.maximumMirror(new int[] { 1, 2, 3, 8,
-                9, 3, 2, 1 }, 8);
+        ArrayFunctions arrayTests = new ArrayFunctions();
+        maximumMirrorValue = arrayTests.maximumMirror(new int[] { 1, 2, 3, 8, 9, 3, 2, 1 }, 8);
         assertEquals(maximumMirrorValue, 3);
     }
 
     @Test
-    public void testMirrorInArray4() {
+    public void testMirrorInArrayWithReturnValue1() {
         int maximumMirrorValue;
-        maximumMirrorValue = arrayTests.maximumMirror(new int[] { 1, 2, 5, 4,
-                2, 7, 9 }, 7);
+        ArrayFunctions arrayTests = new ArrayFunctions();
+        maximumMirrorValue = arrayTests.maximumMirror(new int[] { 1, 2, 5, 4, 2, 7, 9 }, 7);
         assertEquals(maximumMirrorValue, 1);
     }
 
     @Test
-    public void testClumps1() {
+    public void testClumpsWithReturnValue2() {
         int clumpCount;
+        ArrayFunctions arrayTests = new ArrayFunctions();
         clumpCount = arrayTests.countClumps(new int[] { 1, 2, 2, 3, 3 }, 5);
         assertEquals(clumpCount, 2);
     }
 
     @Test
-    public void testClumps2() {
+    public void testClumpsWithReturnValue1() {
         int clumpCount;
+        ArrayFunctions arrayTests = new ArrayFunctions();
         clumpCount = arrayTests.countClumps(new int[] { 1, 1, 1, 1 }, 4);
         assertEquals(clumpCount, 1);
     }
 
     @Test
-    public void testClumps3() {
+    public void testClumpsWithReturnValueTwo() {
         int clumpCount;
+        ArrayFunctions arrayTests = new ArrayFunctions();
         clumpCount = arrayTests.countClumps(new int[] { 1, 1, 2, 1, 1 }, 5);
         assertEquals(clumpCount, 2);
     }
 
     @Test
-    public void testClumps4() {
+    public void testClumpsWithReturnValue0() {
         int clumpCount;
+        ArrayFunctions arrayTests = new ArrayFunctions();
         clumpCount = arrayTests.countClumps(new int[] { 1, 2, 4, 5, 8 }, 5);
         assertEquals(clumpCount, 0);
     }
 
     @Test
-    public void testFixingXYInArray1() {
+    public void testFixingXYInArrayWith2XYPair() {
         int[] actualResult;
+        ArrayFunctions arrayTests = new ArrayFunctions();
         actualResult = new int[] { 9, 4, 5, 4, 5, 9 };
-        int[] expectedResult1 = arrayTests.fixingInstance(new int[] { 5, 4, 9,
-                4, 9, 5 }, 6, 4, 5);
-        assertArrayEquals(expectedResult1, actualResult);
+        int[] expectedResult = arrayTests.fixingInstance(new int[] { 5, 4, 9, 4, 9, 5 }, 6, 4, 5);
+        assertArrayEquals(expectedResult, actualResult);
     }
 
     @Test
-    public void testFixingXYInArray2() {
+    public void testFixingXYInArrayWith1XYPair() {
         int[] actualResult;
+        ArrayFunctions arrayTests = new ArrayFunctions();
         actualResult = new int[] { 1, 4, 5, 1 };
-        int[] expectedResult2 = arrayTests.fixingInstance(new int[] { 1, 4, 5,
-                1 }, 4, 4, 5);
-        assertArrayEquals(expectedResult2, actualResult);
+        int[] expectedResult = arrayTests.fixingInstance(new int[] { 1, 4, 5, 1 }, 4, 4, 5);
+        assertArrayEquals(expectedResult, actualResult);
     }
 
     @Test
-    public void testFixingXYInArray3() {
+    public void testFixingXYInArrayWithX4Y5() {
         int[] actualResult;
+        ArrayFunctions arrayTests = new ArrayFunctions();
         actualResult = new int[] { 1, 4, 5, 1, 1, 4, 5 };
-        int[] expectedResult3 = arrayTests.fixingInstance(new int[] { 1, 4, 1,
-                5, 5, 4, 1 }, 7, 4, 5);
-        assertArrayEquals(expectedResult3, actualResult);
+        int[] expectedResult = arrayTests.fixingInstance(new int[] { 1, 4, 1, 5, 5, 4, 1 }, 7, 4, 5);
+        assertArrayEquals(expectedResult, actualResult);
     }
 
     @Test
-    public void testSplitArray1() {
+    public void testSplitArrayWithReturnValue3() {
         int result;
+        ArrayFunctions arrayTests = new ArrayFunctions();
         result = arrayTests.splittingArray(new int[] { 1, 2, 3, 2, 4 }, 5);
         assertEquals(result, 3);
     }
 
     @Test
-    public void testSplitArray2() {
+    public void testSplitArrayWithReturnValueThree() {
         int result;
+        ArrayFunctions arrayTests = new ArrayFunctions();
         result = arrayTests.splittingArray(new int[] { 1, 1, 1, 2, 1 }, 5);
         assertEquals(result, 3);
     }
 
     @Test
-    public void testSplitArray3() {
+    public void testSplitArrayWithReturnValue-1() {
         int result;
+        ArrayFunctions arrayTests = new ArrayFunctions();
         result = arrayTests.splittingArray(new int[] { 2, 1, 1, 2, 1 }, 5);
         assertEquals(result, -1);
     }
