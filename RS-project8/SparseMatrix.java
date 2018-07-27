618c5579239608194bb3f191331f156e9eed2280
meta-somi-vishnoi <41181551+meta-somi-vishnoi@users.noreply.github.com>
2018-07-27 03:27:04 -0700

    Update SparseMatrix.java

--- RS-project8/SparseMatrix.java
+++ RS-project8/SparseMatrix.java
@@ -4,9 +4,6 @@ final public class SparseMatrix {
     private int noOfColumns;
     private int sparseRepresentation[][];
 
-    SparseMatrix() {
-    }
-
     SparseMatrix(int[][] sparseMatrix) {
         int countNonZeroElements = 0;
         for (int i = 0; i < sparseMatrix.length; i++) {
@@ -16,8 +13,8 @@ final public class SparseMatrix {
                 }
             }
         }
-        this.noOfRows = sparseMatrix.length;
-        this.noOfColumns = sparseMatrix[0].length;
+        noOfRows = sparseMatrix.length;
+        noOfColumns = sparseMatrix[0].length;
         sparseRepresentation = new int[3][countNonZeroElements];
         noOfNonZeroElements = countNonZeroElements;
         int sparseIndex = 0;
@@ -39,20 +36,20 @@ final public class SparseMatrix {
      * @param matrix
      * @return transposeMatrix
      */
-    public int[][] transposeOfMatrix(SparseMatrix matrix) {
-        int[][] transposeRepresentation = new int[3][matrix.noOfNonZeroElements];
-        for (int i = 0; i < matrix.noOfNonZeroElements; i++) {
-            transposeRepresentation[0][i] = matrix.sparseRepresentation[1][i];
-            transposeRepresentation[1][i] = matrix.sparseRepresentation[0][i];
-            transposeRepresentation[2][i] = matrix.sparseRepresentation[2][i];
+    public int[][] transposeOfMatrix() {
+        int[][] transposeRepresentation = new int[3][this.noOfNonZeroElements];
+        for (int i = 0; i < this.noOfNonZeroElements; i++) {
+            transposeRepresentation[0][i] = this.sparseRepresentation[1][i];
+            transposeRepresentation[1][i] = this.sparseRepresentation[0][i];
+            transposeRepresentation[2][i] = this.sparseRepresentation[2][i];
         }
-        int[][] transposeMatrix = new int[matrix.noOfColumns][matrix.noOfRows];
-        for (int i = 0; i < matrix.noOfColumns; i++) {
-            for (int j = 0; j < matrix.noOfRows; j++) {
+        int[][] transposeMatrix = new int[this.noOfColumns][this.noOfRows];
+        for (int i = 0; i < this.noOfColumns; i++) {
+            for (int j = 0; j < this.noOfRows; j++) {
                 transposeMatrix[i][j] = 0;
             }
         }
-        for (int i = 0; i < matrix.noOfNonZeroElements; i++) {
+        for (int i = 0; i < this.noOfNonZeroElements; i++) {
             transposeMatrix[transposeRepresentation[0][i]][transposeRepresentation[1][i]] = transposeRepresentation[2][i];
         }
         return transposeMatrix;
@@ -63,16 +60,16 @@ final public class SparseMatrix {
      * 
      * @param matrix
      */
-    public boolean symmetricMatrix(SparseMatrix matrix) {
-        if (matrix.noOfRows != matrix.noOfColumns) {
+    public boolean isSymmetricMatrix() {
+        if (this.noOfRows != this.noOfColumns) {
             return false;
         }
-        int[][] transposeRepresentationOfMatrix = transposeOfMatrix(matrix);
-        for (int i = 0; i < matrix.noOfNonZeroElements; i++) {
+        for (int i = 0; i < this.noOfNonZeroElements; i++) {
             int found = 0;
-            for (int j = 0; j < matrix.noOfNonZeroElements; j++) {
-                if (matrix.sparseRepresentation[0][i] == matrix.sparseRepresentation[1][j] && matrix.sparseRepresentation[1][i] 
-                == matrix.sparseRepresentation[0][j] && matrix.sparseRepresentation[2][i] == matrix.sparseRepresentation[2][j]) {
+            for (int j = 0; j < this.noOfNonZeroElements; j++) {
+                if (this.sparseRepresentation[0][i] == this.sparseRepresentation[1][j] && 
+                    this.sparseRepresentation[1][i] == this.sparseRepresentation[0][j] && 
+                    this.sparseRepresentation[2][i] == this.sparseRepresentation[2][j]) {
                     found = 1;
                     break;
                 }
@@ -89,39 +86,39 @@ final public class SparseMatrix {
     /**
      * calculates addition of two given matrix
      * 
-     * @param firstMatrix
-     *            , secondMatrix
+     * @param firstMatrix, secondMatrix
      * @return addedMatrix
      */
-    public int[][] addMatrix(SparseMatrix firstMatrix, SparseMatrix secondMatrix) {
-        if (firstMatrix.noOfRows == secondMatrix.noOfRows && firstMatrix.noOfColumns == secondMatrix.noOfColumns) {
-            int[][] addCount = new int[3][firstMatrix.sparseRepresentation[0].length + secondMatrix.sparseRepresentation[0].length];
+    public int[][] addMatrix(SparseMatrix firstMatrix) {
+        if (this.noOfRows == firstMatrix.noOfRows && this.noOfColumns == firstMatrix.noOfColumns) {
+            int[][] addCount = new int[3][this.sparseRepresentation[0].length + 
+                firstMatrix.sparseRepresentation[0].length];
             int indexCount = 0;
-            for (indexCount = 0; indexCount < firstMatrix.sparseRepresentation[0].length; indexCount++) {
-                addCount[0][indexCount] = firstMatrix.sparseRepresentation[0][indexCount];
-                addCount[1][indexCount] = firstMatrix.sparseRepresentation[1][indexCount];
-                addCount[2][indexCount] = firstMatrix.sparseRepresentation[2][indexCount];
+            for (indexCount = 0; indexCount < this.sparseRepresentation[0].length; indexCount++) {
+                addCount[0][indexCount] = this.sparseRepresentation[0][indexCount];
+                addCount[1][indexCount] = this.sparseRepresentation[1][indexCount];
+                addCount[2][indexCount] = this.sparseRepresentation[2][indexCount];
             }
-            for (int j = 0; j < secondMatrix.sparseRepresentation[0].length; j++) {
+            for (int j = 0; j < firstMatrix.sparseRepresentation[0].length; j++) {
                 int flag = 0;
                 for (int i = 0; i < indexCount; i++) {
-                    if (secondMatrix.sparseRepresentation[0][j] == addCount[0][i] 
-                        && secondMatrix.sparseRepresentation[1][j] == addCount[1][i]) {
-                        addCount[2][i] += secondMatrix.sparseRepresentation[2][j];
-                        flag = 1;
-                        break;
+                    if (firstMatrix.sparseRepresentation[0][j] == addCount[0][i] && 
+                        firstMatrix.sparseRepresentation[1][j] == addCount[1][i]) {
+                            addCount[2][i] += firstMatrix.sparseRepresentation[2][j];
+                            flag = 1;
+                            break;
                     }
                 }
                 if (flag == 0) {
-                    addCount[0][indexCount] = secondMatrix.sparseRepresentation[0][j];
-                    addCount[1][indexCount] = secondMatrix.sparseRepresentation[1][j];
-                    addCount[2][indexCount] = secondMatrix.sparseRepresentation[2][j];
+                    addCount[0][indexCount] = firstMatrix.sparseRepresentation[0][j];
+                    addCount[1][indexCount] = firstMatrix.sparseRepresentation[1][j];
+                    addCount[2][indexCount] = firstMatrix.sparseRepresentation[2][j];
                     indexCount++;
                 }
             }
-            int[][] addedMatrix = new int[firstMatrix.noOfRows][firstMatrix.noOfColumns];
-            for (int i = 0; i < firstMatrix.noOfRows; i++) {
-                for (int j = 0; j < firstMatrix.noOfRows; j++) {
+            int[][] addedMatrix = new int[this.noOfRows][this.noOfColumns];
+            for (int i = 0; i < this.noOfRows; i++) {
+                for (int j = 0; j < this.noOfRows; j++) {
                     addedMatrix[i][j] = 0;
                 }
             }
@@ -130,36 +127,35 @@ final public class SparseMatrix {
             }
             return addedMatrix;
         } else {
-            throw new AssertionError();
+            throw new AssertionError("Rows and column mismatch.");
         }
     }
 
     /**
      * calculates multiplication of two given matrix
      * 
-     * @param firstMatrix
-     *            , secondMatrix
+     * @param firstMatrix, secondMatrix
      * @return multiplyMatrix
      */
-    public int[][] multiplyMatrix(SparseMatrix firstMatrix, SparseMatrix secondMatrix) {
-        if (firstMatrix.noOfRows == secondMatrix.noOfColumns && firstMatrix.noOfColumns == secondMatrix.noOfRows) {
-            int[][] multiplyMatrix = new int[firstMatrix.noOfRows][secondMatrix.noOfColumns];
-            for (int i = 0; i < firstMatrix.noOfRows; i++) {
-                for (int j = 0; j < secondMatrix.noOfColumns; j++) {
+    public int[][] multiplyMatrix(SparseMatrix firstMatrix) {
+        if (this.noOfRows == firstMatrix.noOfColumns && this.noOfColumns == firstMatrix.noOfRows) {
+            int[][] multiplyMatrix = new int[this.noOfRows][firstMatrix.noOfColumns];
+            for (int i = 0; i < this.noOfRows; i++) {
+                for (int j = 0; j < firstMatrix.noOfColumns; j++) {
                     multiplyMatrix[i][j] = 0;
                 }
             }
-            for (int i = 0; i < firstMatrix.sparseRepresentation[0].length; i++) {
-                for (int j = 0; j < secondMatrix.sparseRepresentation[0].length; j++) {
-                    if (firstMatrix.sparseRepresentation[1][i] == secondMatrix.sparseRepresentation[0][j]) {
-                        multiplyMatrix[firstMatrix.sparseRepresentation[0][i]][secondMatrix.sparseRepresentation[1][j]] 
-                        += (firstMatrix.sparseRepresentation[2][i] * secondMatrix.sparseRepresentation[2][j]);
+            for (int i = 0; i < this.sparseRepresentation[0].length; i++) {
+                for (int j = 0; j < firstMatrix.sparseRepresentation[0].length; j++) {
+                    if (this.sparseRepresentation[1][i] == firstMatrix.sparseRepresentation[0][j]) {
+                        multiplyMatrix[this.sparseRepresentation[0][i]][firstMatrix.sparseRepresentation[1][j]] += 
+                        (this.sparseRepresentation[2][i] * firstMatrix.sparseRepresentation[2][j]);
                     }
                 }
             }
             return multiplyMatrix;
         } else {
-            throw new AssertionError();
+            throw new AssertionError("Rows and column mismatch.");
         }
     }
 }
