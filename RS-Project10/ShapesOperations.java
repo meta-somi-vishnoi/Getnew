9b2249adbf761502f4dced8efae4b5c4b0e8549f
meta-somi-vishnoi <41181551+meta-somi-vishnoi@users.noreply.github.com>
2018-08-01 03:31:04 -0700

    Update ShapesOperations.java

--- RS-Project10/ShapesOperations.java
+++ RS-Project10/ShapesOperations.java
@@ -1,4 +1,4 @@
-package Graphics;
+package graphics;
 
 import java.util.*;
 import java.sql.*;
@@ -11,9 +11,9 @@ public class ShapesOperations {
      * @param x
      *            coordinate of point, y coordinate of point
      */
-    public boolean checkCoordinates(int xCoordinate, int yCoordinate) {
-        if (xCoordinate >= 0 && xCoordinate <= Shapes.XMAX && yCoordinate >= 0 && 
-            yCoordinate <= Shapes.YMAX) {
+    public boolean checkCoordinates(double xCoordinate, double yCoordinate) {
+        if (xCoordinate >= 0 && xCoordinate <= Shape.XMAX && yCoordinate >= 0 && 
+            yCoordinate <= Shape.YMAX) {
             return true;
         }
         return false;
@@ -23,7 +23,7 @@ public class ShapesOperations {
      * display shapes of various types
      * @param list of shapes
      */
-    public void displayShapes(ArrayList<Shapes> listOfShapes) {
+    public void displayShapes(ArrayList<Shape> listOfShapes) {
         for (int i = 0; i < listOfShapes.size(); i++) {
             System.out.print("Index: " + (i + 1) + "\t");
             listOfShapes.get(i).getInformation();
@@ -34,12 +34,12 @@ public class ShapesOperations {
      * sorts shapes of various types using area
      * @param list of shapes
      */
-    public void sortUsingArea(ArrayList<Shapes> listOfShapes) {
-        ArrayList<Shapes> listOfSortedShapes = new ArrayList<Shapes>(listOfShapes);
+    public void sortUsingArea(ArrayList<Shape> listOfShapes) {
+        ArrayList<Shape> listOfSortedShapes = new ArrayList<Shape>(listOfShapes);
         for (int i = 0; i < (listOfSortedShapes.size() - 1); i++) {
             for (int j = 0; j < (listOfSortedShapes.size() - i - 1); j++) {
                 if (listOfSortedShapes.get(j).getArea() > listOfSortedShapes.get(j + 1).getArea()) {
-                    Shapes shape = listOfSortedShapes.get(j);
+                    Shape shape = listOfSortedShapes.get(j);
                     listOfSortedShapes.set(j, listOfSortedShapes.get(j + 1));
                     listOfSortedShapes.set(j + 1, shape);
                 }
@@ -52,12 +52,12 @@ public class ShapesOperations {
      * sorts shapes of various types using perimeter
      * @param list of shapes
      */
-    public void sortUsingPerimeter(ArrayList<Shapes> listOfShapes) {
-        ArrayList<Shapes> listOfSortedShapes = new ArrayList<Shapes>(listOfShapes);
+    public void sortUsingPerimeter(ArrayList<Shape> listOfShapes) {
+        ArrayList<Shape> listOfSortedShapes = new ArrayList<Shape>(listOfShapes);
         for (int i = 0; i < listOfSortedShapes.size() - 1; i++) {
             for (int j = 0; j < listOfSortedShapes.size() - i - 1; j++) {
                 if (listOfSortedShapes.get(j).getPerimeter() > listOfSortedShapes.get(j + 1).getPerimeter()) {
-                    Shapes shape = listOfSortedShapes.get(j);
+                    Shape shape = listOfSortedShapes.get(j);
                     listOfSortedShapes.set(j, listOfSortedShapes.get(j + 1));
                     listOfSortedShapes.set(j + 1, shape);
                 }
@@ -69,14 +69,14 @@ public class ShapesOperations {
      * sorts shapes of various types using timestamp
      * @param list of shapes
      */
-    public void sortUsingTimestamp(ArrayList<Shapes> listOfShapes) {
-        ArrayList<Shapes> listOfSortedShapes = new ArrayList<Shapes>(listOfShapes);
+    public void sortUsingTimestamp(ArrayList<Shape> listOfShapes) {
+        ArrayList<Shape> listOfSortedShapes = new ArrayList<Shape>(listOfShapes);
         for (int i = 0; i < listOfSortedShapes.size() - 1; i++) {
             for (int j = 0; j < listOfSortedShapes.size() - i - 1; j++) {
                 Timestamp firstShapeTimestamp = listOfSortedShapes.get(j).getTimestamp();
                 Timestamp secondShapeTimestamp = listOfSortedShapes.get(j).getTimestamp();
                 if (firstShapeTimestamp.after(secondShapeTimestamp)) {
-                    Shapes shape = listOfSortedShapes.get(j);
+                    Shape shape = listOfSortedShapes.get(j);
                     listOfSortedShapes.set(j, listOfSortedShapes.get(j + 1));
                     listOfSortedShapes.set(j + 1, shape);
                 }
@@ -88,8 +88,8 @@ public class ShapesOperations {
      * sorts shapes of various types using distance from origin
      * @param list of shapes
      */
-    public void sortUsingOriginDistance(ArrayList<Shapes> listOfShapes) {
-        ArrayList<Shapes> listOfSortedShapes = new ArrayList<Shapes>(listOfShapes);
+    public void sortUsingOriginDistance(ArrayList<Shape> listOfShapes) {
+        ArrayList<Shape> listOfSortedShapes = new ArrayList<Shape>(listOfShapes);
         for (int i = 0; i < listOfSortedShapes.size() - 1; i++) {
             for (int j = 0; j < listOfSortedShapes.size() - i - 1; j++) {
                 Point firstShapeOrigin = listOfSortedShapes.get(j).getOrigin();
@@ -103,7 +103,7 @@ public class ShapesOperations {
                 double distanceOfSecondShape = Math.sqrt(Math.pow(xCoordinateSecondShape, 2) +
                     Math.pow(yCoordinateSecondShape, 2));
                 if (distanceOfFirstShape > distanceOfSecondShape) {
-                    Shapes shape = listOfSortedShapes.get(j);
+                    Shape shape = listOfSortedShapes.get(j);
                     listOfSortedShapes.set(j, listOfSortedShapes.get(j + 1));
                     listOfSortedShapes.set(j + 1, shape);
                 }
@@ -116,8 +116,8 @@ public class ShapesOperations {
      * @param list of shapes, point
      * @return list of shapes enclosing given point
      */
-    public ArrayList<Shapes> getShapesEnclosingPoint(ArrayList<Shapes> listOfShapes, Point point) {
-        ArrayList<Shapes> listOfShapesEnclosingPoint = new ArrayList<Shapes>();
+    public ArrayList<Shape> getShapesEnclosingPoint(ArrayList<Shape> listOfShapes, Point point) {
+        ArrayList<Shape> listOfShapesEnclosingPoint = new ArrayList<Shape>();
         for (int i = 0; i < listOfShapes.size(); i++) {
             if (listOfShapes.get(i).isPointEnclosed(point)) {
                 listOfShapesEnclosingPoint.add(listOfShapes.get(i));
@@ -131,8 +131,8 @@ public class ShapesOperations {
      * @param list of shapes, index of shape
      * @return list of shapes
      */
-    public ArrayList<Shapes> getAboveShapes(ArrayList<Shapes> listOfShapes, int index) {
-        ArrayList<Shapes> listOfShapesAbove = new ArrayList<Shapes>();
+    public ArrayList<Shape> getAboveShapes(ArrayList<Shape> listOfShapes, int index) {
+        ArrayList<Shape> listOfShapesAbove = new ArrayList<Shape>();
         for (int i = 0; i < listOfShapes.size(); i++) {
             if (listOfShapes.get(i).getTimestamp().after(listOfShapes.get(index).getTimestamp())) {
                 listOfShapesAbove.add(listOfShapes.get(i));
