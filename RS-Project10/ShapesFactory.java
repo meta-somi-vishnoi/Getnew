58c5fa997fdd83389ebc0a144d82b6d4f3d29719
meta-somi-vishnoi <41181551+meta-somi-vishnoi@users.noreply.github.com>
2018-07-31 22:54:04 -0700

    Update ShapesFactory.java

--- RS-Project10/ShapesFactory.java
+++ RS-Project10/ShapesFactory.java
@@ -1,22 +1,43 @@
 package Graphics;
+
 import java.util.*;
 
+/**
+ * creates object of class Shapes
+ */
 public class ShapesFactory {
+    
+    /**
+     * create shapes of various types
+     * @param shape type, point coordinates as origin, list of parameters
+     * @return shape
+     */
     public static Shapes createShape(String shapeType, Point point, ArrayList<Integer> list) {
         Shapes shape = null;
-        if(Shapes.ShapesType.Rectangle.equals(shapeType)) {
-            shape=new Rectangle(point, list.get(0), list.get(1));
+        if (Shapes.ShapesType.Rectangle.toString().equals(shapeType)) {
+            shape = new Rectangle(point, list.get(0), list.get(1));
+        } else if (Shapes.ShapesType.Square.toString().equals(shapeType)) {
+            shape = new Square(point, list.get(0));
+        } else if (Shapes.ShapesType.Circle.toString().equals(shapeType)) {
+            shape = new Circle(point, list.get(0));
+        } else {
+            System.out.println("Shape is inavlid");
+            System.exit(0);
         }
-        else if(Shapes.ShapesType.Square.equals(shapeType)) {
-            shape=new Square(point, list.get(2));
-        }
-        else if(Shapes.ShapesType.Triangle.equals(shapeType)) {
-            shape=new Triangle(point, list.get(3));
-        }
-        else if(Shapes.ShapesType.Circle.equals(shapeType)) {
-            shape=new Circle(point, list.get(4));
-        }
-        else {
+        return shape;
+    }
+
+    /**
+     * create shapes of triangle types
+     * @param shape type, point coordinates as origin and point opposite to base , list of parameters
+     * @return shape
+     */
+    public static Shapes createTriangle
+        (String shapeType, Point point, Point pointOppositeToBase, ArrayList<Integer> list) {
+        Shapes shape = null;
+        if (Shapes.ShapesType.Triangle.toString().equals(shapeType)) {
+            shape = new Triangle(point, pointOppositeToBase, list.get(0));
+        } else {
             System.out.println("Shape is inavlid");
             System.exit(0);
         }
