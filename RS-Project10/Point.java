f81e85eb30d3bb1e18572ccf008f6c63732af5ce
meta-somi-vishnoi <41181551+meta-somi-vishnoi@users.noreply.github.com>
2018-07-31 22:52:27 -0700

    Update Point.java

--- RS-Project10/Point.java
+++ RS-Project10/Point.java
@@ -1,15 +1,21 @@
 package Graphics;
 
+/**
+ * create point of coordinate system
+ */
 public class Point {
     private int xCoordinate;
     private int yCoordinate;
+
     Point(int xCoordinate, int yCoordinate) {
-        this.xCoordinate=xCoordinate;
-        this.yCoordinate=yCoordinate;
+        this.xCoordinate = xCoordinate;
+        this.yCoordinate = yCoordinate;
     }
+
     public int getXCoordinate() {
         return xCoordinate;
     }
+
     public int getYCoordinate() {
         return yCoordinate;
     }
