417bb7fbe70ee8959fbb0b8d4c9aac94f87bf598
meta-somi-vishnoi <41181551+meta-somi-vishnoi@users.noreply.github.com>
2018-08-02 04:39:53 -0700

    Add files via upload

---
+++ RS-Project11/Node/Node.java
@@ -0,0 +1,11 @@
+package stackAndQueueImplementation;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int data, Node next) {
+        this.data = data;
+        this.next = next;
+    }
+}
