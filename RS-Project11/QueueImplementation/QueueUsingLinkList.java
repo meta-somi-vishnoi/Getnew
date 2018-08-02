14d92f2e3d97a50798f4ddf031e25de5b209baa1
meta-somi-vishnoi <41181551+meta-somi-vishnoi@users.noreply.github.com>
2018-08-02 04:34:47 -0700

    Update QueueUsingLinkList.java

--- RS-Project11/QueueImplementation/QueueUsingLinkList.java
+++ RS-Project11/QueueImplementation/QueueUsingLinkList.java
@@ -1,40 +1,37 @@
-package queueImplementation;
-class Node {
-    Integer data;
-    Node next;
-}
-public class QueueUsingLinkList implements QueueInterface<Integer>{
+package stackAndQueueImplementation;
+public class QueueUsingLinkList implements QueueInterface<Integer> {
     Node front;
     Node rear;
+
     QueueUsingLinkList() {
-        front=null;
-        rear=null;
+        front = null;
+        rear = null;
     }
+
     public Integer addItemToQueue(Integer item) {
-        Node newNode = new Node();
-        newNode.data=item;
-        newNode.next=null;
-        if(front==null && rear==null) {
-            front=newNode;
-            rear=newNode;
-        }
-        else {
-            front.next=newNode;
-            front=front.next;
+        Node newNode = new Node(item,null);
+        if (front == null && rear == null) {
+            front = newNode;
+            rear = newNode;
+        } else {
+            front.next = newNode;
+            front = front.next;
         }
         return front.data;
-    } 
+    }
+
     public Integer deleteItemFromQueue() {
         Node delete = null;
-        delete=rear;
-        rear=rear.next;
-        if(rear==null) {
-            front=null;
+        delete = rear;
+        rear = rear.next;
+        if (rear == null) {
+            front = null;
         }
         return delete.data;
     }
+
     public boolean isQueueEmpty() {
-        if(front==null && rear==null) {
+        if (front == null && rear == null) {
             return true;
         }
         return false;
