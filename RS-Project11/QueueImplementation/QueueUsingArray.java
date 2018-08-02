2c339f95bec20486672b288ea9fe3cd1f10f9c11
meta-somi-vishnoi <41181551+meta-somi-vishnoi@users.noreply.github.com>
2018-08-02 04:33:31 -0700

    Update QueueUsingArray.java

--- RS-Project11/QueueImplementation/QueueUsingArray.java
+++ RS-Project11/QueueImplementation/QueueUsingArray.java
@@ -1,52 +1,70 @@
-package queueImplementation;
+package stackAndQueueImplementation;
 
-public class QueueUsingArray implements QueueInterface<Integer>{
-    private Integer front;
-    private Integer rear;
-    private Integer[] queue;
-    QueueUsingArray() {
-        queue=new Integer[5];
-        front=-1;
-        rear=-1;
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class QueueUsingArrayTest {
+    QueueUsingArray queue;
+
+    @Before
+    public void testStackArray() {
+        queue = new QueueUsingArray();
+        queue.addItemToQueue(4);
+        queue.addItemToQueue(7);
+        queue.addItemToQueue(8);
     }
-    public Integer addItemToQueue(Integer item) {
-        if(isQueueFull()) {
-            return -1;
-        }
-        else {
-            if(front==-1 && rear==-1) {
-                front++;
-                rear++;
-                queue[front]=item;
-            }
-            else {
-                front++;
-                queue[front]=item;
-            }
-            return queue[front];
-        }
+
+    @Test
+    public void testQueueAddItem3() {
+        assertEquals((int) queue.addItemToQueue(3), 3);
     }
-    public Integer deleteItemFromQueue() {
-        if(isQueueEmpty()) {
-            System.out.println("Queue is empty");
-            return -1;
-        }
-        else {
-            Integer deletedItem=queue[rear];
-            rear++;
-            return deletedItem;
-        }
+
+    @Test
+    public void testIsQueueEmptyFalse() {
+        assertEquals(queue.isQueueEmpty(), false);
     }
-    public boolean isQueueFull() {
-        if(front==(queue.length-1)) {
-            return true;
-        }
-        return false;
+
+    @Test
+    public void testQueueAddItem9() {
+        assertEquals((int) queue.addItemToQueue(9), 9);
     }
-    public boolean isQueueEmpty() {
-        if((front==rear && front==-1) || (rear>front)) {
-            return true;
-        }
-        return false;
+
+    @Test
+    public void testQueueAddItem10() {
+        assertNotEquals((int) queue.addItemToQueue(10), 17);
+    }
+
+    @Test
+    public void testIsQueueFullFalse() {
+        assertEquals(queue.isQueueFull(), false);
+    }
+
+    @Test
+    public void testQueueDeleteItem4() {
+        assertEquals((int) queue.deleteItemFromQueue(), 4);
+    }
+
+    @Test
+    public void testQueueDeleteItem7() {
+        assertEquals((int) queue.deleteItemFromQueue(), 4);
+        assertEquals((int) queue.deleteItemFromQueue(), 7);
+    }
+
+    @Test
+    public void testIsQueueEmptyTrue() {
+        assertEquals((int) queue.deleteItemFromQueue(), 4);
+        assertEquals((int) queue.deleteItemFromQueue(), 7);
+        assertEquals(queue.isQueueEmpty(), false);
+        assertEquals((int) queue.deleteItemFromQueue(), 8);
+        assertEquals(queue.isQueueEmpty(), true);
+    }
+
+    @Test
+    public void testIsQueueFullTrue() {
+        assertEquals((int) queue.addItemToQueue(9), 9);
+        assertEquals((int) queue.addItemToQueue(9), 9);
+        assertEquals(queue.isQueueFull(), true);
     }
 }
