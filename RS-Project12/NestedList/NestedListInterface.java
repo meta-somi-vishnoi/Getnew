f76df803cf19a1bba00710f5d4764295c377d342
meta-somi-vishnoi <41181551+meta-somi-vishnoi@users.noreply.github.com>
2018-08-06 05:31:53 -0700

    Add files via upload

---
+++ RS-Project12/NestedList/NestedListInterface.java
@@ -0,0 +1,11 @@
+package nestedlist;
+
+import java.util.List;
+
+interface NestedListInterface {
+    public Integer sumOfList(List list);
+
+    public boolean isValuePresent(List<Object> list, Integer value);
+
+    public Integer getMaximumValue(List list);
+}
