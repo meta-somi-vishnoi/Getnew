786957ef740ab76b563f2d1fb4b2ff0403b619be
meta-somi-vishnoi <41181551+meta-somi-vishnoi@users.noreply.github.com>
2018-08-09 02:44:20 -0700

    Update DictionaryImplementation.java

--- RS/RS-Project15/DictionaryImplementation/DictionaryImplementation.java
+++ RS/RS-Project15/DictionaryImplementation/DictionaryImplementation.java
@@ -1,91 +1,88 @@
-package dictionary;
+package dictionarymanagement;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
+import static org.junit.Assert.*;
+
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
 import java.util.*;
-import java.util.TreeMap;
-import java.util.Map.Entry;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import javax.json.Json;
-import javax.json.JsonObject;
-import javax.json.JsonReader;
 
-public class DictionaryImplementation implements Dictionary {
-    public Map<String, String> tree;
+public class DictionaryImplementationTest {
+    public final String FILENAME = "C:\\Users\\user39\\Downloads\\Meaning.json";
+    DictionaryImplementation operation;
 
-    DictionaryImplementation() {
-        tree = new TreeMap<String, String>();
+    @Before
+    public void testAddDictionaryEntry() {
+        operation = new DictionaryImplementation();
+        operation.addDictionaryEntry(FILENAME);
     }
 
-    public void addDictionaryEntry(String fileName) {
-        try {
-            if (fileName == null) {
-                System.out.println("File not Found");
-            }
-            File jsonInputFile = new File(fileName);
-            InputStream input = new FileInputStream(jsonInputFile);
-            JsonReader reader = Json.createReader(input);
-            JsonObject jsonObj = reader.readObject();
-            reader.close();
-            Set<String> keys = jsonObj.keySet();
-            Iterator<String> i = keys.iterator();
-            while (i.hasNext()) {
-                String key = (String) i.next();
-                tree.put(key, jsonObj.get(key).toString());
-            }
-        } catch (FileNotFoundException ex) {
-            System.out.println("File not Found in addList");
+    @Test(expected = AssertionError.class)
+    public void testGetValueOfKey() {
+        operation = new DictionaryImplementation();
+        assertEquals("Anger", operation.deleteDictionaryEntry("Fright"));
+    }
+
+    @Test
+    public void testDeleteDromTreePanache() {
+        operation = new DictionaryImplementation();
+        assertEquals("Panache", operation.deleteDictionaryEntry("Panache"));
+    }
+
+    @Test
+    public void testDeleteDromTreeUncanny() {
+        operation = new DictionaryImplementation();
+        assertEquals("Uncanny", operation.deleteDictionaryEntry("Uncanny"));
+    }
+
+    @Test
+    public void testDeleteDromTreeFrown() {
+        operation = new DictionaryImplementation();
+        assertEquals("Frown", operation.deleteDictionaryEntry("Frown"));
+    }
+
+    @Test
+    public void testGetValueOfKeyPanache() {
+        operation = new DictionaryImplementation();
+        assertEquals("Approved", operation.getValueOfKey("Panache"));
+    }
+
+    @Test
+    public void testGetValueOfKeyFalseCase() {
+        operation = new DictionaryImplementation();
+        assertNotEquals("Quiet", operation.getValueOfKey("Frown"));
+    }
+
+    @Test
+    public void testGetValueOfKeyFrown() {
+        operation = new DictionaryImplementation();
+        assertEquals("Anger", operation.getValueOfKey("Frown"));
+    }
+
+    @Test
+    public void testSortKeyValue() {
+        operation = new DictionaryImplementation();
+        Map<String, String> tree = operation.sortDictionaryByKey();
+        ArrayList<String> keyArray = new ArrayList<String>(Arrays.asList("Frown", "Panache", "Peace", "Uncanny"));
+        ArrayList<String> dataArray = new ArrayList<String>(Arrays.asList("Anger", "Approved", "Quiet", "Normal"));
+        int index = 0;
+        for (String key : tree.keySet()) {
+            assertEquals(keyArray.get(index), key);
+            assertEquals(dataArray.get(index), tree.get(key));
+            index++;
         }
     }
 
-    public String deleteDictionaryEntry(String key) {
-        if (key != null) {
-            tree.remove(key);
-            return key;
-        } else {
-            throw new NullPointerException("Key is null");
+    @Test
+    public void testSortKeyValueWithSpecifiedKey() {
+        operation = new DictionaryImplementation();
+        ArrayList<String> tree = operation.sortDictionaryWithKeyValueSpecified("Panache", "Unacnny");
+        ArrayList<String> keyArray = new ArrayList<String>(Arrays.asList("Panache", "Peace", "Uncanny"));
+        int index = 0;
+        for (String key : tree) {
+            assertEquals(keyArray.get(index), key);
+            index++;
         }
-    }
-
-    public String getValueOfKey(String key) {
-        if (key != null) {
-            return tree.get(key);
-        } else {
-
-            throw new NullPointerException("Key is null");
-        }
-    }
-
-    public Map<String, String> sortKeyValue() {
-        Set<Entry<String, String>> set = tree.entrySet();
-        List<Entry<String, String>> list = new ArrayList<Entry<String, String>>(set);
-        Collections.sort(list, new Comparator<Map.Entry<String, String>>() {
-            public int compare(Map.Entry<String, String> firstWord, Map.Entry<String, String> secondWord) {
-                if (secondWord.getValue() != null && firstWord.getValue() != null) {
-                    return (secondWord.getValue()).compareTo(firstWord.getValue());
-                } else {
-                    throw new NullPointerException("Value is null for some key");
-                }
-            }
-        });
-        return tree;
-    }
-
-    public ArrayList<String> sortWithKeyValueSpecified(String smallerKey, String largerKey) {
-        if (smallerKey.compareTo(largerKey) > 0) {
-            throw new AssertionError("Larger key is smaller tha smaller key");
-        }
-        sortKeyValue();
-        ArrayList<String> listOfValues = new ArrayList<String>();
-        for (Map.Entry<String, String> entry : tree.entrySet()) {
-            if (entry.getKey().compareTo(smallerKey) >= 0 && entry.getKey().compareTo(largerKey) <= 0 && entry.getKey() != null) {
-                listOfValues.add(entry.getKey());
-            } else {
-                throw new NullPointerException("Value is null for some key");
-            }
-        }
-        return listOfValues;
     }
 }
