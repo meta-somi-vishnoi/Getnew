81298d13910553756ef74e9a722ddb83d79d1d75
meta-somi-vishnoi <41181551+meta-somi-vishnoi@users.noreply.github.com>
2018-08-11 02:44:52 +0530

    Update GraphImplementation.java

--- GraphImplementation.java
+++ GraphImplementation.java
@@ -2,34 +2,59 @@ package undirectedweightedgraphimplementation;
 
 import java.util.ArrayList;
 
-public class GraphImplementation {
+public class GraphImplementation implements Graph {
+
     public int[][] graphMatrix;
+
     GraphOperations operationOnGraph = new GraphOperations();
+
     GraphImplementation(int[][] graph) {
         graphMatrix = graph;
     }
-    
+
     public boolean isConnected() {
-        return operationOnGraph.depthFirstSearchTraversal(graphMatrix);
+        int[] visitedNode = operationOnGraph.depthFirstSearchTraversal(graphMatrix);
+        int countOfNodeTraversed = 0;
+        for (int i = 0; i < visitedNode.length; i++) {
+            if (visitedNode[i] == 1) {
+                countOfNodeTraversed++;
+            }
+        }
+        if (countOfNodeTraversed == graphMatrix.length) {
+            return true;
+        } else {
+            return false;
+        }
     }
+
     public ArrayList<Integer> reachable(int node) {
         ArrayList<Integer> connectedNodes = new ArrayList<Integer>();
-        for(int i=0;i<graphMatrix.length;i++) {
-            if(i==node) {
-                continue;
+        if (isConnected()) {
+            for (int i = 0; i < graphMatrix.length; i++) {
+                if (i == node) {
+                    continue;
+                } else {
+                    connectedNodes.add(i);
+                }
             }
-            else {
-                if(graphMatrix[node][i]!=0) {
+        } else {
+            int[] visitedNode = operationOnGraph.depthFirstSearchTraversal(graphMatrix);
+            for (int i = 0; i < graphMatrix.length; i++) {
+                if (i == node) {
+                    continue;
+                } else if (visitedNode[i] == 1) {
                     connectedNodes.add(i);
                 }
             }
         }
         return connectedNodes;
     }
+
     public ArrayList<ArrayList<Integer>> minimumSpanningTree() {
         return operationOnGraph.minimumSpanningTree(graphMatrix);
     }
-    public int[] shortestPath(int startNode, int endNode) {
-        return operationOnGraph.dijkstraPath(graphMatrix);       
+
+    public ArrayList<ArrayList<Integer>> shortestPath(int startNode, int endNode) {
+        return operationOnGraph.dijkstraPath(graphMatrix, startNode, endNode);
     }
 }
