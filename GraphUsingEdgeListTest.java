26002a84cee3f7ca7c4d5b29990806e7aa90e7a9
meta-somi-vishnoi <41181551+meta-somi-vishnoi@users.noreply.github.com>
2018-08-11 21:48:47 +0530

    Update GraphUsingEdgeListTest.java

--- GraphUsingEdgeListTest.java
+++ GraphUsingEdgeListTest.java
@@ -1,54 +1,102 @@
 package undirectedweightedgraphimplementation;
 
 import static org.junit.Assert.*;
-
 import java.util.ArrayList;
 import java.util.Arrays;
-
 import org.junit.Before;
 import org.junit.Test;
 
-public class GraphUsingEdgeListTest {
-    GraphImplementation graph;
-    @Test
-    public void testIsConnected() {
-        graph = new GraphImplementation(new int[][]{{0,5,9,0,7,0},{5,0,8,3,0,0},{9,8,0,1,2,0},{0,3,1,0,4,12},{7,0,2,4,0,6},{0,0,0,12,6,0}});
-        assertEquals(true,graph.isConnected());
+public class GraphUsingEdgeListImplementationTest {
+
+    GraphUsingEdgeListImplementation graph;
+
+    @Before
+    public void test() {
+        GraphNode top = null, last;
+        ArrayList<GraphNode> list = new ArrayList<GraphNode>();
+        last = new GraphNode(4, 7, null);
+        top = last;
+        last = new GraphNode(2, 9, top);
+        top = last;
+        last = new GraphNode(1, 5, top);
+        list.add(last);
+        last = new GraphNode(3, 3, null);
+        top = last;
+        last = new GraphNode(2, 8, top);
+        top = last;
+        last = new GraphNode(0, 5, top);
+        list.add(last);
+        last = new GraphNode(4, 2, null);
+        top = last;
+        last = new GraphNode(3, 1, top);
+        top = last;
+        last = new GraphNode(1, 8, top);
+        top = last;
+        last = new GraphNode(0, 9, top);
+        list.add(last);
+        last = new GraphNode(5, 12, null);
+        top = last;
+        last = new GraphNode(4, 4, top);
+        top = last;
+        last = new GraphNode(2, 1, top);
+        top = last;
+        last = new GraphNode(1, 3, top);
+        list.add(last);
+        last = new GraphNode(5, 6, null);
+        top = last;
+        last = new GraphNode(3, 4, top);
+        top = last;
+        last = new GraphNode(2, 2, top);
+        top = last;
+        last = new GraphNode(0, 7, top);
+        list.add(last);
+        last = new GraphNode(4, 6, null);
+        top = last;
+        last = new GraphNode(3, 12, top);
+        list.add(last);
+        graph = new GraphUsingEdgeListImplementation(list);
     }
+
     @Test
     public void testIsConnectedTrue() {
-        graph = new GraphImplementation(new int[][]{{0,5,9,0,7,0},{5,0,8,3,0,0},{0,8,0,0,2,0},{0,3,1,0,4,12},{7,0,2,4,0,6},{0,0,0,12,6,0}});
-        assertEquals(true,graph.isConnected());
+        assertEquals(true, graph.isConnected());
     }
+
     @Test
     public void testIsConnectedFalse() {
-        graph = new GraphImplementation(new int[][]{{0,0,9,0,7,0},{0,0,0,0,0,0},{0,0,0,0,2,0},{0,0,1,0,4,12},{7,0,2,4,0,6},{0,0,0,12,6,0}});
-        assertEquals(false,graph.isConnected());
+        assertNotEquals(false, graph.isConnected());
     }
+
     @Test
     public void testReachableFromNode4() {
-        graph = new GraphImplementation(new int[][]{{0,5,9,0,7,0},{5,0,8,3,0,0},{9,8,0,1,2,0},{0,3,1,0,4,12},{7,0,2,4,0,6},{0,0,0,12,6,0}});
-        assertEquals(new ArrayList<Integer>(Arrays.asList(0,2,3,5)),graph.reachable(4));
+        assertEquals(new ArrayList<Integer>(Arrays.asList(0, 1, 2, 3, 5)), graph.reachable(4));
     }
-    @Test
-    public void testReachableFromNode0() {
-        graph = new GraphImplementation(new int[][]{{0,5,9,0,7,0},{5,0,8,3,0,0},{9,8,0,1,2,0},{0,3,1,0,4,12},{7,0,2,4,0,6},{0,0,0,12,6,0}});
-        assertEquals(new ArrayList<Integer>(Arrays.asList(1,2,4)),graph.reachable(0));
-    }
-    @Test
-    public void testReachableFromNode5() {
-        graph = new GraphImplementation(new int[][]{{0,5,9,0,7,0},{5,0,8,3,0,0},{9,8,0,1,2,0},{0,3,1,0,4,12},{7,0,2,4,0,6},{0,0,0,12,6,0}});
-        assertEquals(new ArrayList<Integer>(Arrays.asList(3,4)),graph.reachable(5));
-    }
+
     @Test
     public void testMinimumSpanningTree() {
-        graph = new GraphImplementation(new int[][]{{0,1,1,0,1,0},{1,0,1,1,0,0},{1,1,0,1,1,0},{0,1,1,0,1,1},{1,0,1,1,0,1},{0,0,0,1,1,0}});
         ArrayList<ArrayList<Integer>> expectedlistOfPath = new ArrayList<ArrayList<Integer>>();
-        expectedlistOfPath.add(new ArrayList<Integer>(Arrays.asList(0,1)));
-    //    ArrayList<ArrayList<Integer>> listOfNodesTraversed = graph.minimumSpanningTree();
-        System.out.println(graph.minimumSpanningTree());
-      //  for(int i=0;i<listOfNodesTraversed.size();i++) {
-     //       assertEquals
-     //   }
+        expectedlistOfPath.add(new ArrayList<Integer>(Arrays.asList(0, 1)));
+        expectedlistOfPath.add(new ArrayList<Integer>(Arrays.asList(1, 3)));
+        expectedlistOfPath.add(new ArrayList<Integer>(Arrays.asList(3, 2)));
+        expectedlistOfPath.add(new ArrayList<Integer>(Arrays.asList(2, 4)));
+        expectedlistOfPath.add(new ArrayList<Integer>(Arrays.asList(4, 5)));
+        ArrayList<ArrayList<Integer>> listOfNodesTraversed = graph.minimumSpanningTree();
+        for (int i = 0; i < listOfNodesTraversed.size(); i++) {
+            assertEquals(listOfNodesTraversed.get(i), expectedlistOfPath.get(i));
+        }
+    }
+
+    @Test
+    public void testShortestPath() {
+        ArrayList<ArrayList<Integer>> expectedlistOfPath = new ArrayList<ArrayList<Integer>>();
+        expectedlistOfPath.add(new ArrayList<Integer>(Arrays.asList(0, 1)));
+        expectedlistOfPath.add(new ArrayList<Integer>(Arrays.asList(0, 4)));
+        expectedlistOfPath.add(new ArrayList<Integer>(Arrays.asList(1, 3)));
+        expectedlistOfPath.add(new ArrayList<Integer>(Arrays.asList(0, 2)));
+        expectedlistOfPath.add(new ArrayList<Integer>(Arrays.asList(4, 5)));
+        ArrayList<ArrayList<Integer>> listOfNodesTraversed = graph.shortestPath(0, 5);
+        for (int i = 0; i < listOfNodesTraversed.size(); i++) {
+            assertEquals(listOfNodesTraversed.get(i), expectedlistOfPath.get(i));
+        }
     }
 }
