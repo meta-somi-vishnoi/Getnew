6bf0cb00268dc8badb516402ed18af4b4780ee25
meta-somi-vishnoi <41181551+meta-somi-vishnoi@users.noreply.github.com>
2018-08-11 21:47:38 +0530

    Update GraphUsingEdgeListImplementation.java

--- GraphUsingEdgeListImplementation.java
+++ GraphUsingEdgeListImplementation.java
@@ -2,27 +2,58 @@ package undirectedweightedgraphimplementation;
 
 import java.util.ArrayList;
 
-public class GraphUsingEdgeListImplementation {
-    public ArrayList<ArrayList<Integer>> graphEdgeList;
+public class GraphUsingEdgeListImplementation implements Graph {
+
+    public ArrayList<GraphNode> graphEdgeList;
     GraphEdgeListOperations operationOnGraph = new GraphEdgeListOperations();
-    GraphUsingEdgeListImplementation(ArrayList<ArrayList<Integer>> graph) {
+
+    GraphUsingEdgeListImplementation(ArrayList<GraphNode> graph) {
         graphEdgeList = graph;
     }
-    
+
     public boolean isConnected() {
-        return operationOnGraph.depthFirstSearchTraversal(graphEdgeList);
+        int[] visitedNode = operationOnGraph.depthFirstSearchTraversal(graphEdgeList);
+        int countOfNodeTraversed = 0;
+        for (int i = 0; i < visitedNode.length; i++) {
+            if (visitedNode[i] == 1) {
+                countOfNodeTraversed++;
+            }
+        }
+        if (countOfNodeTraversed == graphEdgeList.size()) {
+            return true;
+        } else {
+            return false;
+        }
     }
+
     public ArrayList<Integer> reachable(int node) {
         ArrayList<Integer> connectedNodes = new ArrayList<Integer>();
-        for(int i=0;i<graphEdgeList.get(node).size();i++) {
-            connectedNodes.add(graphEdgeList.get(node).get(i));
+        if (isConnected()) {
+            for (int i = 0; i < graphEdgeList.size(); i++) {
+                if (i == node) {
+                    continue;
+                } else {
+                    connectedNodes.add(i);
+                }
+            }
+        } else {
+            int[] visitedNode = operationOnGraph.depthFirstSearchTraversal(graphEdgeList);
+            for (int i = 0; i < graphEdgeList.size(); i++) {
+                if (i == node) {
+                    continue;
+                } else if (visitedNode[i] == 1) {
+                    connectedNodes.add(i);
+                }
+            }
         }
         return connectedNodes;
     }
+
     public ArrayList<ArrayList<Integer>> minimumSpanningTree() {
         return operationOnGraph.minimumSpanningTree(graphEdgeList);
     }
+
     public ArrayList<ArrayList<Integer>> shortestPath(int startNode, int endNode) {
-        return operationOnGraph.dijkstraPath(graphEdgeList);       
+        return operationOnGraph.dijkstraPath(graphEdgeList, startNode, endNode);
     }
 }
