114c92a9179338c1f7dc49860073731645d2887e
meta-somi-vishnoi <41181551+meta-somi-vishnoi@users.noreply.github.com>
2018-09-04 03:13:45 -0700

    Add files via upload

---
+++ SpringPersonSingletonScope/com/metacube/training/MainApp.java
@@ -0,0 +1,16 @@
+package com.metacube.training;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+          Person person = (Person) context.getBean("person");
+          person.checkType();
+          person.setType("Admin");
+          person.checkType();
+          Person otherPerson = (Person) context.getBean("person");
+          otherPerson.checkType();
+    }
+}
