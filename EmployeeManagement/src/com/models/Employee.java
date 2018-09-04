a8c6ac1dc211546f022e041ef46ac1eeec867d70
meta-somi-vishnoi <41181551+meta-somi-vishnoi@users.noreply.github.com>
2018-09-04 01:43:12 -0700

    Update Employee.java

--- EmployeeManagement/src/com/models/Employee.java
+++ EmployeeManagement/src/com/models/Employee.java
@@ -6,7 +6,8 @@ public class Employee {
     private String lastName;
     private String email;
     private int age;
-    public Employee(String firstName, String lastName, String email, int age) {
+    public Employee(int id, String firstName, String lastName, String email, int age) {
+        this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
