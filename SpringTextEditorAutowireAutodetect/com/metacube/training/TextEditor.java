81260d090911996ddd8c3eeaa2f690f22da52296
meta-somi-vishnoi <41181551+meta-somi-vishnoi@users.noreply.github.com>
2018-09-04 03:25:22 -0700

    Add files via upload

---
+++ SpringTextEditorAutowireAutodetect/com/metacube/training/TextEditor.java
@@ -0,0 +1,19 @@
+package com.metacube.training;
+
+public class TextEditor {
+    private SpellChecker spellChecker;
+
+    // a constructor method to inject the dependency.
+    public TextEditor(SpellChecker spellChecker) {
+        this.spellChecker = spellChecker;
+    }
+
+    // a getter method to return spellChecker
+    public SpellChecker getSpellChecker() {
+        return spellChecker;
+    }
+
+    public void spellCheck(String word) {
+        spellChecker.checkSpelling(word);
+    }
+}
