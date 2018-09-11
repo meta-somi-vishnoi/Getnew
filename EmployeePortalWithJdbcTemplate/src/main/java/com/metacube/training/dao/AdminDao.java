ae2f9c5baa66db4e2b813a0f4db61142bcdcb957
meta-somi-vishnoi <41181551+meta-somi-vishnoi@users.noreply.github.com>
2018-09-11 06:05:33 -0700

    Update AdminDao.java

--- EmployeePortalWithJdbcTemplate/src/main/java/com/metacube/training/dao/AdminDao.java
+++ EmployeePortalWithJdbcTemplate/src/main/java/com/metacube/training/dao/AdminDao.java
@@ -41,6 +41,10 @@ public class AdminDao {
         return jdbcTemplate.query(Queries.GET_ALL_EMPLOYEES, new EmployeeMapper());
     }
 
+    public Employee getEmployeeByUsername(String email, String password) {
+        return jdbcTemplate.queryForObject(Queries.GET_EMPLOYEE_BY_USERNAME, new Object[] { email, password }, new EmployeeMapper());
+    }
+    
     public Employee getEmployeeByCode(int code) {
         return jdbcTemplate.queryForObject(Queries.GET_EMPLOYEE_BY_CODE, new Object[] { code }, new EmployeeMapper());
     }
@@ -50,7 +54,7 @@ public class AdminDao {
     }
 
     public List<Employee> searchEmployees(String firstName, String lastName) {
-        return (List<Employee>) jdbcTemplate.queryForObject(Queries.SEARCH_EMPLOYEES, new Object[] { firstName, lastName },
+        return jdbcTemplate.query(Queries.SEARCH_EMPLOYEES, new Object[]{firstName, lastName},
             new EmployeeMapper());
     }
 
@@ -71,6 +75,15 @@ public class AdminDao {
             return Status.DELETED;
         } else {
             return Status.NOT_DELETED;
+        }
+    }
+    
+    public Status updatePassword(String email, String password) {
+    	boolean result = jdbcTemplate.update(Queries.UPDATE_PASSWORD, password, email) > 0;
+    	if(result == true) {
+    		return Status.UPDATED;
+    	}else {
+            return Status.NOT_UPDATED;
         }
     }
 }
