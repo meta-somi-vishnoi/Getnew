6910ad1364570c4e3ade60a8f0cf8928edea5a37
meta-somi-vishnoi <41181551+meta-somi-vishnoi@users.noreply.github.com>
2018-09-11 06:03:35 -0700

    Update JobService.java

--- EmployeePortalWithJdbcTemplate/src/main/java/com/metacube/training/services/JobService.java
+++ EmployeePortalWithJdbcTemplate/src/main/java/com/metacube/training/services/JobService.java
@@ -11,44 +11,42 @@ import com.metacube.training.dao.JobDao;
 import com.metacube.training.dao.ProjectDao;
 import com.metacube.training.models.JobTitle;
 import com.metacube.training.models.Project;
+
 @Service
 public class JobService {
 
-	@Autowired
-	private JobDao jobDao;
+    @Autowired
+    private JobDao jobDao;
 
-	public Status addJob(JobTitle job) {
+    public Status addJob(JobTitle job) {
         Status status = jobDao.addJob(job);
         return status;
     }
-	
-	public JobTitle getJobByCode(int code) {
-		return jobDao.getJobByCode(code);
-	}
 
-	public List<JobTitle> getAllJobs() {
-		return jobDao.getAllJobs();
-	}
+    public JobTitle getJobByCode(int code) {
+        return jobDao.getJobByCode(code);
+    }
 
-	public Status deleteJob(int code) {
-		JobTitle job = jobDao.getJobByCode(code);
-		if(job==null) {
-			return Status.NOT_EXIST;
-		} else {
-			Status status = jobDao.deleteJob(job);
-			return status;
-		}
-	}
+    public List<JobTitle> getAllJobs() {
+        return jobDao.getAllJobs();
+    }
 
-	public Status updateJob(JobTitle job) {
-		List<JobTitle> listOfJob = new ArrayList<JobTitle>();
-        listOfJob = jobDao.getAllJobs();
-        for (JobTitle existingJob : listOfJob) {
-            if (job.getJobCode()== existingJob.getJobCode()) {
-                Status status = jobDao.updateJob(job);
-                return status;
-            }
-        } 
+    public Status deleteJob(int code) {
+        JobTitle job = jobDao.getJobByCode(code);
+        if (job == null) {
+            return Status.NOT_EXIST;
+        } else {
+            Status status = jobDao.deleteJob(job);
+            return status;
+        }
+    }
+
+    public Status updateJob(JobTitle job) {
+        JobTitle existingJob = jobDao.getJobByCode(job.getJobCode());
+        if (existingJob != null) {
+            Status status = jobDao.updateJob(job);
+            return status;
+        }
         return Status.NOT_EXIST;
-	}
+    }
 }
