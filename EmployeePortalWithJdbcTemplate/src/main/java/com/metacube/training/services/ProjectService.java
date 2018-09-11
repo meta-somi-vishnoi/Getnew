f70eb4d1f41adc80194ee3948e2611c20c655ecd
meta-somi-vishnoi <41181551+meta-somi-vishnoi@users.noreply.github.com>
2018-09-11 06:03:58 -0700

    Update ProjectService.java

--- EmployeePortalWithJdbcTemplate/src/main/java/com/metacube/training/services/ProjectService.java
+++ EmployeePortalWithJdbcTemplate/src/main/java/com/metacube/training/services/ProjectService.java
@@ -15,41 +15,38 @@ import com.metacube.training.models.Project;
 @Service
 public class ProjectService {
 
-	@Autowired
-	private ProjectDao projectDao;
+    @Autowired
+    private ProjectDao projectDao;
 
-	public Status addProject(Project project) {
+    public Status addProject(Project project) {
         Status status = projectDao.addProject(project);
         return status;
     }
-	
-	public Project getProjectById(int id) {
-		return projectDao.getProjectById(id);
-	}
 
-	public List<Project> getAllProjects() {
-		return projectDao.getAllProjects();
-	}
+    public Project getProjectById(int id) {
+        return projectDao.getProjectById(id);
+    }
 
-	public Status deleteProject(int id) {
-		Project project = projectDao.getProjectById(id);
-		if(project==null) {
-			return Status.NOT_EXIST;
-		} else {
-			Status status = projectDao.deleteProject(project);
-			return status;
-		}
-	}
+    public List<Project> getAllProjects() {
+        return projectDao.getAllProjects();
+    }
 
-	public Status updateProject(Project project) {
-		List<Project> listOfProject = new ArrayList<Project>();
-        listOfProject = projectDao.getAllProjects();
-        for (Project existingProject : listOfProject) {
-            if (project.getProjectId()== existingProject.getProjectId()) {
-                Status status = projectDao.updateProject(project);
-                return status;
-            }
-        } 
+    public Status deleteProject(int id) {
+        Project project = projectDao.getProjectById(id);
+        if (project == null) {
+            return Status.NOT_EXIST;
+        } else {
+            Status status = projectDao.deleteProject(project);
+            return status;
+        }
+    }
+
+    public Status updateProject(Project project) {
+        Project projectPresent = projectDao.getProjectById(project.getProjectId());
+        if (projectPresent != null) {
+            Status status = projectDao.updateProject(project);
+            return status;
+        }
         return Status.NOT_EXIST;
-	}
+    }
 }
