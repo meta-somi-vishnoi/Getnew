235bcf5f3b80bb9d968cd69483fe661f53677045
meta-somi-vishnoi <41181551+meta-somi-vishnoi@users.noreply.github.com>
2018-09-11 06:04:19 -0700

    Update SkillsService.java

--- EmployeePortalWithJdbcTemplate/src/main/java/com/metacube/training/services/SkillsService.java
+++ EmployeePortalWithJdbcTemplate/src/main/java/com/metacube/training/services/SkillsService.java
@@ -12,41 +12,38 @@ import com.metacube.training.models.Skills;
 
 @Service
 public class SkillsService {
-	@Autowired
-	private SkillsDao skillsDao;
+    @Autowired
+    private SkillsDao skillsDao;
 
-	public Status addSkill(Skills skill) {
+    public Status addSkill(Skills skill) {
         Status status = skillsDao.addSkill(skill);
         return status;
     }
-	
-	public Skills getSkillById(int id) {
-		return skillsDao.getSkillById(id);
-	}
 
-	public List<Skills> getAllSkills() {
-		return skillsDao.getAllSkills();
-	}
+    public Skills getSkillById(int id) {
+        return skillsDao.getSkillById(id);
+    }
 
-	public Status deleteSkill(int id) {
-		Skills skill = skillsDao.getSkillById(id);
-		if(skill==null) {
-			return Status.NOT_EXIST;
-		} else {
-			Status status = skillsDao.deleteSkill(skill);
-			return status;
-		}
-	}
+    public List<Skills> getAllSkills() {
+        return skillsDao.getAllSkills();
+    }
 
-	public Status updateSkill(Skills skill) {
-		List<Skills> listOfSkills = new ArrayList<Skills>();
-        listOfSkills = skillsDao.getAllSkills();
-        for (Skills existingSkill : listOfSkills) {
-            if (skill.getSkillId()== existingSkill.getSkillId()) {
-                Status status = skillsDao.updateSkill(skill);
-                return status;
-            }
-        } 
+    public Status deleteSkill(int id) {
+        Skills skill = skillsDao.getSkillById(id);
+        if (skill == null) {
+            return Status.NOT_EXIST;
+        } else {
+            Status status = skillsDao.deleteSkill(skill);
+            return status;
+        }
+    }
+
+    public Status updateSkill(Skills skill) {
+        Skills skillPresent = skillsDao.getSkillById(skill.getSkillId());
+        if (skillPresent != null) {
+            Status status = skillsDao.updateSkill(skill);
+            return status;
+        }
         return Status.NOT_EXIST;
-	}
+    }
 }
