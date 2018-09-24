7b5755f8484800c72f3fc7bca4d21282394a47fe
meta-somi-vishnoi <41181551+meta-somi-vishnoi@users.noreply.github.com>
2018-09-24 02:16:14 -0700

    Add files via upload

---
+++ SpringSecurity/AnnotationConfig/src/main/java/com/metacube/training/services/ProjectService.java
@@ -0,0 +1,52 @@
+package com.metacube.training.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.metacube.training.Status.Status;
+import com.metacube.training.dao.AdminDao;
+import com.metacube.training.dao.ProjectDao;
+import com.metacube.training.models.Employee;
+import com.metacube.training.models.Project;
+
+@Service
+public class ProjectService {
+
+    @Autowired
+    private ProjectDao projectDao;
+
+    public Status addProject(Project project) {
+        Status status = projectDao.addProject(project);
+        return status;
+    }
+
+    public Project getProjectById(int id) {
+        return projectDao.getProjectById(id);
+    }
+
+    public List<Project> getAllProjects() {
+        return projectDao.getAllProjects();
+    }
+
+    public Status deleteProject(int id) {
+        Project project = projectDao.getProjectById(id);
+        if (project == null) {
+            return Status.NOT_EXIST;
+        } else {
+            Status status = projectDao.deleteProject(project);
+            return status;
+        }
+    }
+
+    public Status updateProject(Project project) {
+        Project projectPresent = projectDao.getProjectById(project.getProjectId());
+        if (projectPresent != null) {
+            Status status = projectDao.updateProject(project);
+            return status;
+        }
+        return Status.NOT_EXIST;
+    }
+}
