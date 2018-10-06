1632441e1276e45fc412ac5386dc695bd0645371
meta-somi-vishnoi <41181551+meta-somi-vishnoi@users.noreply.github.com>
2018-10-06 03:04:40 -0700

    Update doublyLinkedList.js

--- UI-Session8/doublyLinkedList.js
+++ UI-Session8/doublyLinkedList.js
@@ -23,22 +23,38 @@ function LinkedList() {
     }
 
     this.removeElement = function(data) { 
-        var current = this.head; 
-        var previousElement = null; 
-        while (current != null) {  
-            if (current.data === data) { 
-                if (previousElement == null) { 
-                    this.head = current.next; 
-                    current.next.previous = null;
-                } else { 
-                    previousElement.next = current.next; 
-                    current.next.previous = previousElement;
+        if(!this.isEmpty()) {
+            var current = this.head; 
+            var previousElement = null; 
+            while (current != null) {  
+                if (current.data === data) { 
+                    if (previousElement == null) { 
+                        this.head = current.next; 
+                        if(current.next!=null) {
+                            current.next.previous = null;
+                        }
+                    } else { 
+                        previousElement.next = current.next; 
+                        if(current.next!=null) {
+                            current.next.previous = previousElement;
+                        }
+                    } 
+                    return current.data; 
                 } 
-                return current.data; 
+                previousElement = current; 
+                current = current.next; 
             } 
-            previousElement = current; 
-            current = current.next; 
-        } 
+            return -1;
+        } else {
+            return -1;
+        }
+    }
+
+    this.isEmpty = function() {
+        if(this.head == null) {
+            return true;
+        }
+        return false;
     }
 
     this.display = function(list) {
@@ -49,4 +65,3 @@ function LinkedList() {
         }
     }
 }
- 
