014ecbf88515d6c821f5db463b1bf5fd8db69afa
meta-somi-vishnoi <41181551+meta-somi-vishnoi@users.noreply.github.com>
2018-10-06 03:14:25 -0700

    Update stack.js

--- UI-Session8/stack.js
+++ UI-Session8/stack.js
@@ -9,11 +9,19 @@ Stack.prototype.pushElement = function(data) {
 }
 
 Stack.prototype.popElement = function() {
-	if (this.elements.length == 0) {
+	if (this.isEmpty()) {
         return "Stack Underflow"; 
 	}
     return this.elements.pop(); 
 }
+
+Stack.prototype.isEmpty = function() {
+	if (this.elements.length == 0) {
+        return true; 
+	}
+	return false;
+}
+
 
 Stack.prototype.display = function() {
     for (var i = 0; i < this.elements.length; i++) { 
